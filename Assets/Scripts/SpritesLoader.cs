b5742fadff62d1f2d8d29c034e56390840f4b201
DmcEx0 <Mizzzlo17@gmail.com>
2024-10-12 12:03:21 +0300

    release operations handles

--- Assets/Scripts/SpritesLoader.cs
+++ Assets/Scripts/SpritesLoader.cs
@@ -1,14 +1,32 @@
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class SpritesLoader : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer[] _sprites;
     [SerializeField] private AssetReference[] _assetReferences;
 
+    private List<AsyncOperationHandle> _operationHandles;
+
+    private void Start()
+    {
+        _operationHandles = new();
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var operationHandle in _operationHandles)
+        {
+            Addressables.Release(operationHandle);
+        }
+    }
+
     public void LoadSprite()
     {
+
         for (int i = 0; i < _sprites.Length; i++)
         {
             LoadSpriteAsync(_sprites[i], i).Forget();
@@ -19,7 +37,8 @@ public class SpritesLoader : MonoBehaviour
     {
         var loadOperation = Addressables.LoadAssetAsync<Sprite>(_assetReferences[number]);
         await loadOperation.ToUniTask();
-        Debug.Log(loadOperation.Result + " - load operation result");
+        
+        _operationHandles.Add(loadOperation);
         
         spriteRenderer.sprite = loadOperation.Result;
     }
